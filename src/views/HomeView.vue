46e21f9c68554a01f1a1ce117fa61992b6a8cbb7
songchengdong35 <576596778@qq.com>
2025-01-16 17:15:00 +0800

    feat: ref

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <button @click="increment">count {{ count }}</button>
   </main>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+let count = ref(0)
+function increment() {
+  count.value++
+  console.log('increment', count.value)
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
